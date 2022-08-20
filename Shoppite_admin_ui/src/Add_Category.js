7fe1a7d837c85193903bd95f95556bbb58b7288c
jeniththakkar007 <jenith.thakkar008@gmail.com>
2022-08-21 00:42:15 +0530

    Shoppite redirect flow

--- Shoppite_admin_ui/src/Add_Category.js
+++ Shoppite_admin_ui/src/Add_Category.js
@@ -1,12 +1,22 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Icon } from '@iconify/react';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import * as Endpoint from './End_point';
  export const Add_Category=()=>{
+	let iconList=[{id:"simple-icons:homeassistant",value:"Home"},
+	{id:"map:electrician",value:"Electronics"},
+	{id:"fluent:food-grains-24-filled",value:"foods"},
+	{id:"ic:round-kitchen",value:"kitchen"},
+	{id:"mdi:face-woman-shimmer",value:"Beauty Product"},
+	{id:"map:clothing-store",value:"Clothes"},
+	{id:"file-icons:ring",value:"Jwellary"},
+	{id:"emojione-monotone:high-heeled-shoe",value:"Footwear"},
+	{id:"bi:bag-plus-fill",value:"Other"}];
+	const navigate = useNavigate();
 	// Post-call for addCategory
 	const PostCategory = () => {
-            
 		(async () => {
 			// POST request using fetch with async/await
 			const requestCOptions = {
@@ -17,18 +27,19 @@ import * as Endpoint from './End_point';
 					"category_code": document.getElementById('c_code').value,
 					"category_name": document.getElementById('c_name').value,
 					"category_description": document.getElementById('c_desc').value,
-					"category_image": document.getElementById('uploadImage').value
+					"category_image": document.getElementById('ctg_type').value
 				}
 				  )
 			};
-			const responseC = await fetch(Endpoint.CATEGORYADD, requestCOptions);
+			const responseC = await fetch(Endpoint.CATEGORYADD, requestCOptions).then(navigate('/category', {replace: true}));
+
 			// const dataU = await responseV.json();
 			// myAlertTop();
 			
 		})();
 	  }
 	useEffect(()=>{
-		pic();
+		//pic();
 	   },[])
 	   // selected pic show on selected location
 	 function pic() {
@@ -87,27 +98,20 @@ import * as Endpoint from './End_point';
 									<input type="text" class="form-control" id="c_code" />
 								</div>
 							</div>
-							<div class=" Logo">
-					<div class="card card_profile">
-						<div class="card-body ">
-						<div className="djs">
-								 Upload Category Picture
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="mb-0"> Category Type</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+								<select class="form-select" id="ctg_type"  aria-label="Default select example">
+									{iconList.map((icon)=>(<>
+										<option value={icon.id}>{icon.value}</option>
+										</>
+									))}
+								</select>
+								</div>
 							</div>
-                            <div className="row mb-3">
-                                <div className="col-sm-3">
-                            <input type="file" id="uploadImage" name="termek_file" class="file_input" multiple/><Tooltip title="Size Must be 223 x 240 px">
-      <IconButton style={{"fontSize":"inherit"}}>
-      <Icon icon="bi:question-diamond-fill" />
-      </IconButton>
-    </Tooltip>
-                            </div>
-  
-    </div>
-  
-
-							</div>
-							</div>
-							</div>
+				
 						</div>
                         <div class="col-sm-9 text-secondary" style={{"marginBottom":"1%"}}>
 									<button type="button" class="btn btn-primary last_btn" onClick={PostCategory}>Add Category</button> 
